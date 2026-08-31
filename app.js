199907cae3680512cd1825a2e77e258445c4f5df
sophiacouto <sophia.8038@colegioprogresso.g12.br>
2026-08-31 08:42:42 -0300

    novas regras

--- app.js
+++ app.js
@@ -1,43 +1,59 @@
-const guerreiro = {
-nome: "Thorin",
-vida: 60,
-ataque: 20,
+class Personagem {
+    constructor(nome, vida, ataque) {
+        this.nome = nome,
+            this.vida = vida,
+            this.ataque = ataque
 
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
-},
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
+    }
+
+    causaDano(personagem) {
+        personagem.recebeDano(this.ataque)
+    }
+
+    recebeDano(quantidade) {
+        this.vida = this.vida - quantidade
+
+        if (this.vida <= 0) {
+            this.vida = "eliminado"
+
+        }
+    }
+      estaVivo(){
+        return this.vida > 0
+      }
+
+      mostrarStatus(){
+console.log(this.nome + " | Vida: " + this.vida)
+}
 }
 
-}
+const guerreiro = new Personagem("Alfarr", 70,20)
 
-const mago = {
-nome: "Gandalfe",
-vida: 60,
-ataque: 35,
+const elfo = new Personagem("Logon", 80, 25)
 
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
-},
+const mago = new Personagem("Lazar", 60, 25)
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
-}
-}
-const arqueiro = {
-nome: "Legolas",
-vida: 80,
-ataque: 25,
+const arqueiro = new Personagem("Apollo", 80, 25)
 
-causaDano: function(personagem){
-personagem.recebeDano(this.ataque)
-},
+const dragão = new Personagem("Diaval", 100, 50)
 
-recebeDano: function(quantidade){
-this.vida = this.vida - quantidade
-}
+const anjo = new Personagem("Muriel", 110, 15)
 
-}
-guerreiro.causaDano(mago)
-console.log(mago.vida)
+anjo.causaDano(elfo)
+console.log("Muriel atacou Logon")
+console.log("Vida do Logon:", elfo.vida)
+
+guereiro.causaDano(arqueiro)
+console.log("Alfarr atacou Apollo")
+console.log("Vida do Apollo:", arqueiro.vida)
+
+mago.causaDano(dragão)
+console.log("Lazar atacou Diaval")
+console.log("Vida do Diaval:", dragão.vida)
+
+
+
+
+
+
+    
